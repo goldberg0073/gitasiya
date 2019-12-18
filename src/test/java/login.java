f14a6e8a4ae7c156a292e81e11ad5a928c6b8c81
goldberg0073 <hicham_goldberg@hotmail.com>
2019-12-18 12:51:02 -0500

    add login.java to loginbranche

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,5 +1,9 @@
 public class login {
     public static void main(String[] args) {
+
         System.out.println("changes now happend in intellij");
+
+System.out.println("changes now happend in github");
+
     }
 }
